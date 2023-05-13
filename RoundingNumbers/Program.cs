821ff1fa8030eefc826e1ec4165c11ff82d648a0
etheoo <etheoo@protonmail.com>
2023-05-13 18:47:38 +0200

    Code cleanup

--- RoundingNumbers/Program.cs
+++ RoundingNumbers/Program.cs
@@ -1,54 +1,47 @@
 ﻿using System.Globalization;
 
-namespace RoundingNumbers
+namespace RoundingNumbers;
+
+internal class RoundingNumbers
 {
-    internal class RoundingNumbers
+    private static void Main()
     {
-        private static void Main()
+        double decimalValue;
+        int decimalLength;
+        var decimalSeparator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+
+        // Read the decimal value from user input
+        do
         {
-            decimal decimalValue;
-            int decimalLength;
-            var decimalSeparator = CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator;
+            Console.Write("Enter a number containing a decimal: ");
+            var input = Console.ReadLine();
 
-            // Read the decimal value from user input
-            do
-            {
-                Console.Write("Enter a number containing a decimal: ");
-                var input = Console.ReadLine();
+            // If input is a valid decimal number, store it and exit loop
+            if (double.TryParse(input!.Replace(",", "."), out decimalValue)) break;
 
-                // If input is a valid decimal number, store it and exit loop
-                if (decimal.TryParse(input!.Replace(",", "."), out decimalValue))
-                {
-                    break;
-                }
+            Console.WriteLine("Error: The input must be a number containing at least one decimal. Please try again.");
+        } while (true);
 
-                Console.WriteLine("Error: The input must be a number containing at least one decimal. Please try again.");
+        // Read the number of decimal places to round to from user input
+        while (true)
+        {
+            Console.Write("Enter the number of decimal places to round off to (1-15): ");
+            var input = Console.ReadLine();
 
-            } while (true);
+            // If input is a valid integer between 1 and 15, store it and exit loop
+            if (int.TryParse(input, out decimalLength) && decimalLength is >= 1 and <= 15) break;
 
-            // Read the number of decimal places to round to from user input
-            while (true)
-            {
-                Console.Write("Enter the number of decimal places to round off to (1-15): ");
-                var input = Console.ReadLine();
-
-                // If input is a valid integer between 1 and 15, store it and exit loop
-                if (int.TryParse(input, out decimalLength) && decimalLength is >= 1 and <= 15)
-                {
-                    break;
-                }
-
-                Console.WriteLine("Error: The input must be a whole number between 1 and 15. Please try again.");
-            }
-
-            // Round the decimal value to the specified number of decimal places
-            string roundedString = Math.Round(decimalValue, decimalLength, MidpointRounding.AwayFromZero).ToString($"F{decimalLength}");
-            
-            // Replace the first occurrence of the period character with the decimal separator for the current culture
-            string output = roundedString.Replace(".", decimalSeparator);
-            
-            // Print the rounded value to the console
-            Console.WriteLine($"Rounded value: {output}");
+            Console.WriteLine("Error: The input must be a whole number between 1 and 15. Please try again.");
         }
+
+        // Round the decimal value to the specified number of decimal places
+        var roundedString = Math.Round(decimalValue, decimalLength, MidpointRounding.AwayFromZero)
+            .ToString($"F{decimalLength}");
+
+        // Replace the first occurrence of the period character with the decimal separator for the current culture
+        var output = roundedString.Replace(".", decimalSeparator);
+
+        // Print the rounded value to the console
+        Console.WriteLine($"Rounded value: {output}");
     }
 }
